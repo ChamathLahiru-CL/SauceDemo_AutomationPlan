8d03d135e72bf9117de6381f1f2e986ae1919951
Lahiru Jayasuriya <chamathjayasuriya2002cmp@gmail.com>
2026-01-29 21:33:09 +0530

    Added the test report option and other unnecessary test files removed. Completed the End to End user flow of the web application.

--- src/test/java/org/testCases/BaseTest.java
+++ src/test/java/org/testCases/BaseTest.java
@@ -1,16 +1,25 @@
 package org.testCases;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.pages.*;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import java.time.Duration;
+
 
 public class BaseTest {
 
     WebDriver driver;
+
+    public ExtentReports extent;
+    public ExtentTest test;
 
 //    Page Object variable for each page
     LoginPage loginPage;
@@ -37,6 +46,16 @@ public class BaseTest {
         // Navigate to the SauceDemo website
         driver.get("https://www.saucedemo.com/");
 
+        //Make Report
+        String path = System.getProperty("user.dir") + "/src/reports/index.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setReportName("Web Automation Results of 'Saucedemo' Application");
+        reporter.config().setDocumentTitle("Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester","Lahiru Jayasuriya");
+
         // Initialize all Page Objects and pass the WebDriver to them
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
@@ -47,9 +66,29 @@ public class BaseTest {
 
     }
 
+    @AfterMethod
+    public void getResult(ITestResult result) {
+
+        if (test == null) {
+            return; // prevents NullPointerException
+        }
+
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, "Test Failed: " + result.getName());
+            test.log(Status.FAIL, result.getThrowable());
+        }
+        else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, "Test Passed: " + result.getName());
+        }
+        else if (result.getStatus() == ITestResult.SKIP) {
+            test.log(Status.SKIP, "Test Skipped: " + result.getName());
+        }
+    }
+
     @AfterTest
     public void tearDown(){
-        // Close browser
+        extent.flush();
         driver.quit();
     }
+
 }
--- src/test/java/org/testCases/HappyPath.java
+++ src/test/java/org/testCases/HappyPath.java
@@ -14,6 +14,8 @@ public class HappyPath extends BaseTest{
     @Test
     public void e2ePurchaseFlow(){
 
+        test = extent.createTest("Test E2E WorkFlow.", "Verify user can login to the system and purchase the products.");
+
         loginPage.loginAccount(uName,uPassword);
         loginPage.clickLoginBTN();
 
@@ -22,7 +24,6 @@ public class HappyPath extends BaseTest{
 
         String headerProducts = productsPage.getHeaderText();
         Assert.assertEquals(headerProducts, "Products");
-        System.out.println("User Login Successfully.");
 
         productsPage.addItemsToCart();
         productsPage.goToCart();
@@ -34,14 +35,11 @@ public class HappyPath extends BaseTest{
 
         String itemTotal = checkOutOverviewPage.getItemTotal();
         Assert.assertTrue(itemTotal.contains("39.98"),"Total price is Incorrect!");
-        System.out.println("Item Total is correct:" + itemTotal);
         checkOutOverviewPage.finishPayment();
 
         String successMSG = checkOutCompletePage.getSuccessMessage();
         Assert.assertEquals(successMSG, "Thank you for your order!");
-        System.out.println("End-to-End (E2E) purchase flow Complete.");
 
         checkOutCompletePage.backHome();
-
     }
 }
